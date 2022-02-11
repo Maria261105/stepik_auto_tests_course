312f5f895d5f5d6090bc07a586e980e8c5601b8f
Mariya <manya_ilupova@mail.ru>
2022-02-11 16:31:40 +0200

    homework from previous module

---
+++ lesson6_step15.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser.get(link)
+    call_alt = browser.find_element_by_xpath("//button[@type='submit']")
+    call_alt.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    answer = browser.find_element_by_id("answer")
+    answer.send_keys(y)
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
